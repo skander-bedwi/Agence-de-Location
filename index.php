7a1f665fed2ea916ce7f5fcda28466b7b5a263da
Skander <skanderbedwi5@gmail.com>
2020-12-15 20:11:24 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Agence</title>
+</head>
+<body>
+<?PHP
+
+$cnx=new PDO('mysql:host=localhost;dbname=agence','root','');
+
+?>
+
+  <nav>
+  	   <label class="LOGO">  DESIGN  </label>
+
+     <ul>
+     	 <li><a class="active" href="#">Home</a></li>
+     	 <li><a href="#"> Alocation Voiture</a></li>
+     	 <li><a href="#"> Contact</a></li>
+
+     </ul>
+
+  </nav> 
+  
+ 
+</body>
+</html>
